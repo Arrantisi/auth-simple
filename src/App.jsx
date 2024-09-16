a5d43b728edc0a16228f4c2dfa542730e0ca5d92
booren <yasin.arran@gmail.com>
2024-09-16 09:07:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Auth from './pages/login/Auth'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Register from './pages/register/Register'
+import Main from './pages/main/Main'
+
+const App = () => {
+
+
+  return (
+
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/login' element={<Auth />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default App
